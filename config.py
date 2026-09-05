97652122ff496e3c24792204c9027556bd161393
vikas-hanje <vikashanje007@gmail.com>
2026-09-06 00:03:37 +0530

    vercel stuff

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ JWT_SECRET_KEY = os.getenv('JWT_SECRETE_KEY')
 
 # Database connection settings
 DB_HOST = os.getenv('DB_host')
+DB_PORT = os.getenv('DB_port')
 DB_USER = os.getenv('DB_user')
 DB_PASSWORD = os.getenv('DB_password')
 DB_DATABASE = os.getenv('DB_database')
--- db.py
+++ db.py
@@ -1,10 +1,13 @@
 import mysql.connector
-from config import DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE
+from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE
 
 
 def connect_DB():
     connection = mysql.connector.connect(
         host = DB_HOST,
+        port= DB_PORT,
+        ssl_verify_cert=True,
+        ssl_verify_identity=True,
         user = DB_USER,
         password = DB_PASSWORD,
         database = DB_DATABASE
